75acd3f09b1da063b00ec7879a9f9e6263695810
Ophelia Boamah Ampoh <ophybee8@gmail.com>
2021-01-08 17:16:08 +0000

    Form UI

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <div className=' w-auto flex justify-center'>
+        <div className='flex w-1/2'>
+          <div className='bg-green-300 text-white flex-1  p-20'>
+            <h3 className='text-4xl font-bold mb-4 text-center mt-8'>
+              Welcome Back!
+            </h3>
+            <p className='text-xl text-center'>
+              To register for our service, kindly provide the necessary details.
             </p>
-          </a>
-        </div>
-      </main>
+          </div>
+          <div className='flex w-1/2'>
+            <div className='bg-gray-100 text-white text-center p-20'>
+              <h2 className='font-bold text-4xl text-green-300 -mt-6 mb-6'>
+                Create Account
+              </h2>
+              <form action='post'>
+                <input
+                  className='mb-3 text-black w-80 h-9 p-2'
+                  type='text'
+                  placeholder='Your first name'
+                />
+                <input
+                  className='mb-3 text-black w-80 h-9 p-2'
+                  type='text'
+                  placeholder='Your last name'
+                />
+                <input
+                  className='mb-3 text-black w-80 h-9 p-2'
+                  type='email'
+                  placeholder='Your email'
+                />
+                <input
+                  className='mb-3 bg-white text-black w-80 h-9 p-2'
+                  type='password'
+                  placeholder='Your password'
+                />
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+                <div className='flex justify-center'>
+                  <button className='text-white px-4 py-2 mt-4 bg-green-300 '>
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>{/* <p className="font-bold">Hey there</p> */}</div>
+  );
+}
+
+export default App;
